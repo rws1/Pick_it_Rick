980fa91622429bd40fead7ad1b8ddf94992297eb
km184 <55809503+km184@users.noreply.github.com>
2019-11-03 14:28:16 +0000

    stage1
    stage1 of rabbit

---
+++ 01/main-01.js
@@ -0,0 +1,51 @@
+window.addEventListener("load", function(event) {
+
+  "use strict";
+
+      ///////////////////
+    //// FUNCTIONS ////
+  ///////////////////
+
+  var render = function() {
+
+    display.renderColor(game.color);
+    display.render();
+
+  };
+
+  var update = function() {
+
+    game.update();
+
+  };
+
+        /////////////////
+      //// OBJECTS ////
+    /////////////////
+
+    /* Usually I just write my logical sections into object literals, but the temptation
+    to reference one inside of another is too great, and leads to sloppy coding.
+    In an effort to attain cleaner code, I have written classes for each section
+    and instantiate them here. */
+
+    /* The controller handles user input. */
+    var controller = new Controller();
+    /* The display handles window resizing, as well as the on screen canvas. */
+    var display    = new Display(document.querySelector("canvas"));
+    /* The game will eventually hold our game logic. */
+    var game       = new Game();
+    /* The engine is where the above three sections can interact. */
+    var engine     = new Engine(1000/30, render, update);
+
+        ////////////////////
+      //// INITIALIZE ////
+    ////////////////////
+
+    window.addEventListener("resize",  display.handleResize);
+    window.addEventListener("keydown", controller.handleKeyDownUp);
+    window.addEventListener("keyup",   controller.handleKeyDownUp);
+
+    display.resize();
+    engine.start();
+
+});
